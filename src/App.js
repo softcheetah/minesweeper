34c96cf1cab1cbaf3d14fd55bd1dc3f81f42145c
wang <cheetah.dev.js@gmail.com>
2021-04-04 11:06:31 +0800

    build basic infrastructure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React from "react";
+/* eslint-disable react/no-array-index-key */
+import React, { useState } from "react";
+import { Field } from "src/components";
+import { width, height } from "src/config";
+import { generateFields } from "src/utility";
+import { STATE } from "src/constant";
 
-function App() {
+const App = () => {
+  const [fields, setFields] = useState(generateFields());
+  const [states, setStates] = useState(
+    new Array(width * height).fill(STATE.INIT)
+  );
+  const [finished, setFinished] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {" "}
-          <code>src/App.js</code>
-          {" "}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="fields-container">
+        { fields.map((field, index) => (
+          <Field
+            key={index}
+            field={field}
+            state={states[index]}
+            finished={finished}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
